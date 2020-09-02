83a1c804f60717527e744733368bc7d88e1f3865
rlawlssn4 <rlawlssn4@naver.com>
2020-09-02 16:38:29 +0900

    2020-09-02

---
+++ 2-3 제어 문자를 사용한 출력.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("be happy\n");
+	printf("1234567890\n");
+	printf("My\tfriend\n");
+	printf("Goot\bd\tchance\n");
+	printf("Cow\rW\n");
+
+	return 0;
+}
