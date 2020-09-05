4bd2f4ad7e8fd6add0657ea86c4705c9736ceb12
rlawlssn4 <rlawlssn4@naver.com>
2020-09-05 23:42:04 +0900

    2020-09-05

--- 2-5 세 가지 진법의 정수 상수.c
+++ 2-5 세 가지 진법의 정수 상수.c
@@ -2,9 +2,10 @@
 
 int main(void)
 {
-	printf("%d\n", 12); //10진수 정수 출력
-	printf("%d\n", 014); //8진수 정수 출력
-	printf("%d\n", 0xc); //16진수 정수 출력
+	printf("%.1lf\n", 1e6);
+	printf("%.7lf\n", 3.14e-5);
+	printf("%le\n", 0.0000314);
+	printf("%.2le\n", 0.0000314);
 
 	return 0;
 }
