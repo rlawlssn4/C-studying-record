b22039fdca403a80f4b79e01b5ea0946ac4c8a00
unknown <rlawlssn4@naver.com>
2020-09-09 21:47:58 +0900

    2020-09-09

---
+++ 3-6 char 배열에 문자열 저장.c
@@ -0,0 +1,4 @@
+﻿1>------ 빌드 시작: 프로젝트: Project1, 구성: Debug Win32 ------
+1>source.c
+1>Project1.vcxproj -> C:\Users\owner\Desktop\C언어\Debug\Project1.exe
+========== 빌드: 성공 1, 실패 0, 최신 0, 생략 0 ==========
